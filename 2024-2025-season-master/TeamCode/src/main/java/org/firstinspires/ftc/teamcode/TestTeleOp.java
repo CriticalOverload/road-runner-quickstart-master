a345b0824f274d0c77807dbe25372079d167f122
criti <critical.overlord@gmail.com>
2024-10-16 17:37:11 -0400

    9/2/2024 - THis is a test for the new repo using an old teleop

--- 2024-2025-season-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ 2024-2025-season-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -1,91 +1,126 @@
 package org.firstinspires.ftc.teamcode;
-// Imports are added automatically when you type in new code
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-//@TeleOp means that this code will appear on the TeleOp dropdown menu when you open the
-//driver station. "Jolie Test" is the name it will display.
-@TeleOp(name = "Jolie TeleOp Test2")
-// If you didn't want this to appear on the list, you add @Disabled
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-// You put extends LinearOpMode at end because it helps us gain access to commands in the FTC SDK
-//for Linear OpModes
-public class TestTeleOp extends LinearOpMode
-{
-    //This declares the motors (like defining variables)
-    private DcMotor motorFrontRight;
-    private DcMotor motorFrontLeft;
-    private DcMotor motorBackRight;
-    private DcMotor motorBackLeft;
+@TeleOp(name = "AA Main TeleOp")
+public class TestTeleOp extends LinearOpMode {
+    private DcMotor motorFrontRight, motorFrontLeft, motorBackLeft, motorBackRight, motorLS, motorLS2, motorJoint;
+    private DistanceSensor distSensor;
+    private Servo servo, servoJoint, servoRoller;  // Added servoRoller
+    private double powerMod = 0.8;
+    private double slidePMod = 1.0;
+    private double value = 1;
 
-    // The runOpMode is required when writing a Linear OpMode
     @Override
-    public void runOpMode() throws InterruptedException
-    {
-        // This code is run when you click init.
+    public void runOpMode() throws InterruptedException {
+        motorFrontRight = hardwareMap.dcMotor.get("FR");
+        motorFrontLeft = hardwareMap.dcMotor.get("FL");
+        motorBackLeft = hardwareMap.dcMotor.get("BL");
+        motorBackRight = hardwareMap.dcMotor.get("BR");
+        motorLS = hardwareMap.dcMotor.get("LS");
+        motorLS2 = hardwareMap.dcMotor.get("LS2");
+        motorJoint = hardwareMap.dcMotor.get("JM");
+        servoJoint = hardwareMap.servo.get("JS");
+        servoRoller = hardwareMap.servo.get("RS");  // Initialize servoRoller
+        distSensor = hardwareMap.get(DistanceSensor.class, "distSensor");
 
-        //This links the variables to the actual motor.
-        motorBackLeft = hardwareMap.dcMotor.get("1");
-        motorBackRight = hardwareMap.dcMotor.get("0");
-        motorFrontLeft = hardwareMap.dcMotor.get("3");
-        motorFrontRight = hardwareMap.dcMotor.get("2");
+        servo.setPosition(0);
 
-
-        //We reverse a motor from each pair so that the robot doesn't spin but actually moves.
+        // Set zero power behavior and direction for motors
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
-        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLS.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLS2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorJoint.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // waitForStart is required so that the code doesn't start running as you click init.
-        //Instead, it waits till you click start
         waitForStart();
 
-        //This code is run after you click Start.
-        //This While loop will run until the stop button is pressed.
+        while(opModeIsActive()) {
+            value = distSensor.getDistance(DistanceUnit.INCH);
+            telemetry.addData("Distance:", value);
 
-        while(opModeIsActive())
-        {
-            //This makes it so that moving the right stick forward or backward moves the robot
-            // forward (motors 1 and 3 turn), and moving the stick to the left or right makes the
-            //robot move sideways (motors 2 and 0 turn). Putting the stick diagonally forward or
-            // backwards makes the robot move diagonally (all motors turn).
-            //There is a negative sign in front of the gamepad1 because moving the joystick forward
-            // actually returns a value of -1 instead of 1, so the robot would actually move
-            // backwards. The negative sign fixes that.
-            motorFrontLeft.setPower(-gamepad1.right_stick_y);
-            motorBackLeft.setPower(-gamepad1.right_stick_y);
-            motorBackRight.setPower(gamepad1.right_stick_x);
-            motorFrontRight.setPower(gamepad1.right_stick_x);
-
-
-            if (gamepad1.left_stick_x == 1) {
-                motorBackRight.setDirection(DcMotor.Direction.FORWARD);
-                motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
-                motorBackLeft.setPower(-1);
-                motorFrontLeft.setPower(-1);
-                motorBackRight.setPower(-1);
-                motorFrontRight.setPower(-1);
-
-            } else if (gamepad1.left_stick_x == -1) {
-                motorBackRight.setDirection(DcMotor.Direction.FORWARD);
-                motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
-                motorBackLeft.setPower(1);
-                motorFrontLeft.setPower(1);
-                motorBackRight.setPower(1);
-                motorFrontRight.setPower(1);
-            }else{
-                motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
-                motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
-                motorFrontRight.setDirection(DcMotor.Direction.FORWARD);
-                motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+            // Power scaling for driving
+            if (gamepad1.right_bumper) {
+                powerMod = 0.5;
+                telemetry.addData("right bumper","gamepad1");
+            } else if (gamepad1.left_bumper) {
+                powerMod = 0.3;
+                telemetry.addData("left bumper","gamepad1");
+            } else {
+                powerMod = 0.8;
             }
 
-            //test
-            // This idle command is like a buffer, and gives the hardware a chance to catch
-            //up to the code.
-            idle();
-        }
+            // Power scaling for linear slides
+            if (gamepad2.right_bumper) {
+                slidePMod = 0.85;
+                telemetry.addData("right bumper","gamepad2");
+            } else if (gamepad2.left_bumper) {
+                slidePMod = 0.35;
+                telemetry.addData("left bumper","gamepad2");
+            } else {
+                slidePMod = 0.6;
+            }
 
+            // Mecanum drive calculations
+            double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) - (Math.PI / 4);
+            double r = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
+            double rotation = gamepad1.left_stick_x;
+
+            double powerOne = r * Math.sin(angle);
+            double powerTwo = r * Math.cos(angle);
+
+            motorFrontLeft.setPower((powerOne - rotation) * powerMod);
+            motorFrontRight.setPower((powerTwo + rotation) * powerMod);
+            motorBackLeft.setPower((powerTwo - rotation) * powerMod);
+            motorBackRight.setPower((powerOne + rotation) * powerMod);
+            motorLS.setPower(gamepad2.right_stick_y * slidePMod);
+            motorLS2.setPower(-gamepad2.right_stick_y * slidePMod); // LS2 mirrors LS
+
+            // Control motorJoint with gamepad2 left stick
+            motorJoint.setPower(gamepad2.left_stick_y * powerMod); // Control motorJoint with left stick y
+
+            // Control servoJoint with dpad buttons or triggers
+            if (gamepad2.dpad_left) {
+                servoJoint.setPosition(0.0);  // Move servoJoint to one extreme
+            } else if (gamepad2.dpad_right) {
+                servoJoint.setPosition(1.0);  // Move servoJoint to the other extreme
+            }
+
+            // Control servoRoller with gamepad2 buttons
+            if (gamepad2.b) {
+                servoRoller.setPosition(1.0);  // Full speed in one direction
+            } else if (gamepad2.x) {
+                servoRoller.setPosition(0.0);  // Stop the roller
+            } else if (gamepad2.y) {
+                servoRoller.setPosition(-1.0);  // Full speed in the opposite direction
+            }
+
+            // Claw control
+            if (gamepad2.dpad_up) {
+                servo.setPosition(0.0);
+            } else if (gamepad2.dpad_down) {
+                servo.setPosition(0.5);
+            }
+
+            if (gamepad2.a) {
+                servo.setPosition(0.0);
+            } else if (gamepad2.y) {
+                servo.setPosition(0.5);
+            }
+
+            telemetry.addData("Slide position", motorLS.getCurrentPosition());
+            telemetry.addData("Slide position", motorLS2.getCurrentPosition());
+            telemetry.update();
+        }
     }
 }
